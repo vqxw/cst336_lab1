1dfe3a2af90e5fb1590eb9d024f8452b26b30c13
Raquel Zempoalteca <rzempoalteca@csumb.edu>
2018-02-01 01:27:30 +0000

    Add all lab 1 files

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<!--
+First comment
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+        <meta charset="utf-8"/>
+        <title>Raquel Zempoalteca: Personal Manifesto</title>
+        <link href="css/styles.css" rel="stylesheet" type="text/css" />
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        <header>
+            <h1>Raquel Zempoalteca</h1>
+        </header>
+        
+        <nav>
+            <hr width = "50%"/>
+            
+            <a href="index.html">Home</a>
+            <a href="about.html">About</a>
+            <a href="contact.html">Contact</a>
+        </nav>
+        
+        <br /><br />
+        
+        <main>
+            <figure id = "me">
+                <!-- "/" in the img src starts at the root of the directory,
+                ex: raquelzempoalteca folder"-->
+                
+                <img width="200px" height = "200px" src="img/juan_doe.png"/>
+            </figure>
+            
+            <div id = "welcomeText">
+                Hello! <br />
+                <p> Thank you for visiting my professional portfolio website. </p>
+                <p> I am a Software Engineer. </p>
+                <p> Feel free to contact me. </p>
+                <br /><br />
+                <em>
+                    "With ordinary talent and extraordinary <strong>preserverance</strong>, all things are attainabl
+                </em><br />
+                - Thomas F. Buxton
+            </div>
+        </main>
+        
+        
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+            <hr>
+            CST 336. 2018&copy; Zempoalteca <br />
+            <strong>Disclaimer: </strong> The information on this webpage is fictitious. <br />
+            It is used for academic purposes only.
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
