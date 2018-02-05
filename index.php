b073e9f2ba8c27fb49de393bc7b483d0800b7009
Raquel Zempoalteca <rzempoalteca5@gmail.com>
2018-02-04 23:53:24 -0800

    better look

--- index.php
+++ index.php
@@ -9,6 +9,8 @@ First comment
     <head>
         <meta charset="utf-8"/>
         <title>Raquel Zempoalteca: Personal Manifesto</title>
+        
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
         <link href="css/styles.css" rel="stylesheet" type="text/css" />
     </head>
 <!-- closing head -->
@@ -17,15 +19,15 @@ First comment
     <!-- This is where we place the content of our website -->
     <body>
         <header>
-            <h1>Raquel Zempoalteca</h1>
+            <h1> Raquel Zempoalteca </h1>
         </header>
         
         <nav>
             <hr width = "50%"/>
             
-            <a href="index.html">Home</a>
-            <a href="about.html">About</a>
-            <a href="contact.html">Contact</a>
+            <a href="index.html"><strong> Home </strong></a>
+            <a href="about.html"> About </a>
+            <a href="contact.html"> Contact </a>
         </nav>
         
         <br /><br />
@@ -35,30 +37,32 @@ First comment
                 <!-- "/" in the img src starts at the root of the directory,
                 ex: raquelzempoalteca folder"-->
                 
-                <img width="200px" height = "200px" src="img/juan_doe.png"/>
+                <img width="150px" height = "235px" src="img/raquel_doe.png"/>
             </figure>
             
             <div id = "welcomeText">
+                <p></p>
                 Hello! <br />
                 <p> Thank you for visiting my professional portfolio website. </p>
-                <p> I am a Software Engineer. </p>
+                <p> I am a CSUMB student with an emphasis in Software Engineering. </p>
                 <p> Feel free to contact me. </p>
                 <br /><br />
                 <em>
-                    "With ordinary talent and extraordinary <strong>preserverance</strong>, all things are attainabl
+                    "It's not the load that breaks you down, it's the way you <strong> carry </strong> it."
                 </em><br />
-                - Thomas F. Buxton
+                - Lou Holtz
             </div>
         </main>
-        
         
         <!-- This is the footer -->
         <!-- The footer goes inside the body but not always -->
         <footer>
             <hr>
             CST 336. 2018&copy; Zempoalteca <br />
-            <strong>Disclaimer: </strong> The information on this webpage is fictitious. <br />
+            <strong> Disclaimer: </strong> The information on this webpage is fictitious. <br />
             It is used for academic purposes only.
+            <p> </p>
+            <img width = "200px" height = "100px" src ="https://breezymartin.files.wordpress.com/2013/10/csumb_logo.jpg">
         </footer>
         <!-- closing footer -->
         
